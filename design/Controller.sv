40576e34b17342c2fae9142af7254415c014b0bc
Andreywid Souza <130323202+Andreywid@users.noreply.github.com>
2024-02-10 20:11:50 -0300

    Outra att para o Jal e Jalr

--- design/Controller.sv
+++ design/Controller.sv
@@ -16,10 +16,12 @@ module Controller (
     output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
     output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic Branch,  //0: branch is not taken; 1: branch is taken
+    output logic Jal,
+    output logic Jalr
 );
 
-  logic [6:0] R_TYPE, I_TYPE, LUI_TYPE, LW, SW, BR;
+  logic [6:0] R_TYPE, I_TYPE, LUI_TYPE, LW, SW, BR, JAL1, Jalr1;
 
   assign R_TYPE = 7'b0110011;  //add,and
   assign I_TYPE = 7'b0010011; // addi
@@ -27,13 +29,17 @@ module Controller (
   assign SW = 7'b0100011;  //sw
   assign BR = 7'b1100011;  //beq
   assign LUI_TYPE = 7'b0110111; // lui
+  assign JAL1 = 7'b1101111; // JAL1
+  assign Jalr1 = 7'b1100111; // Jalr1
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == LUI_TYPE);
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == LUI_TYPE || Opcode == Jalr1);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == LUI_TYPE ||);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == LUI_TYPE || Opcode == Jalr1 || Opcode == JAL1);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
-  assign ALUOp[0] = (Opcode == BR || Opcode == LUI_TYPE);
-  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == LUI_TYPE);
+  assign ALUOp[0] = (Opcode == BR || Opcode == LUI_TYPE || Opcode == Jalr1);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == LUI_TYPE || Opcode == Jalr1);
   assign Branch = (Opcode == BR);
+  assign Jal = (Opcode == JAL1) || (Opcode == Jalr1);
+  assign Jalr = (Opcode == Jalr1);
 endmodule
