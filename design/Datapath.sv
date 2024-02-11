00ed6fa2f55a4b6e410106a7de08288d1f1ed413
Andreywid Souza <130323202+Andreywid@users.noreply.github.com>
2024-02-10 21:26:45 -0300

    Atualização do Datapath

--- design/Datapath.sv
+++ design/Datapath.sv
@@ -13,11 +13,14 @@ module Datapath #(
     input  logic                 clk,
     reset,
     RegWrite,
+    Jal,
     MemtoReg,  // Register file writing enable   // Memory or ALU MUX
     ALUsrc,
     MemWrite,  // Register file or Immediate MUX // Memroy Writing Enable
     MemRead,  // Memroy Reading Enable
-    Branch,  // Branch Enable
+    Branch,// Branch Enable
+    Halt,
+    Jalr, 
     input  logic [          1:0] ALUOp,
     input  logic [ALU_CC_W -1:0] ALU_CC,         // ALU Control Code ( input of the ALU )
     output logic [          6:0] opcode,
@@ -45,6 +48,7 @@ module Datapath #(
   logic [DATA_W-1:0] SrcB, ALUResult;
   logic [DATA_W-1:0] ExtImm, BrImm, Old_PC_Four, BrPC;
   logic [DATA_W-1:0] WrmuxSrc;
+  logic [DATA_W-1:0] temp;
   logic PcSel;  // mux select / flush signal
   logic [1:0] FAmuxSel;
   logic [1:0] FBmuxSel;
@@ -135,12 +139,15 @@ module Datapath #(
     if ((reset) || (Reg_Stall) || (PcSel))   // initialization or flush or generate a NOP if hazard
         begin
       B.ALUSrc <= 0;
+      B.Jal <= 0;
       B.MemtoReg <= 0;
       B.RegWrite <= 0;
       B.MemRead <= 0;
       B.MemWrite <= 0;
       B.ALUOp <= 0;
       B.Branch <= 0;
+      B.Halt <= 0;
+      B.Jalr <= 0;
       B.Curr_Pc <= 0;
       B.RD_One <= 0;
       B.RD_Two <= 0;
@@ -153,12 +160,15 @@ module Datapath #(
       B.Curr_Instr <= A.Curr_Instr;  //debug tmp
     end else begin
       B.ALUSrc <= ALUsrc;
+      B.Jal <= Jal;
       B.MemtoReg <= MemtoReg;
       B.RegWrite <= RegWrite;
       B.MemRead <= MemRead;
       B.MemWrite <= MemWrite;
       B.ALUOp <= ALUOp;
       B.Branch <= Branch;
+      B.Halt <= Halt;
+      B.Jalr <= Jalr;
       B.Curr_Pc <= A.Curr_Pc;
       B.RD_One <= Reg1;
       B.RD_Two <= Reg2;
@@ -221,6 +231,9 @@ module Datapath #(
       B.Curr_Pc,
       B.ImmG,
       B.Branch,
+      B.Halt,
+      B.Jal,
+      B.Jalr,
       ALUResult,
       BrImm,
       Old_PC_Four,
@@ -233,6 +246,7 @@ module Datapath #(
     if (reset)   // initialization
         begin
       C.RegWrite <= 0;
+      C.Jal <= 0;
       C.MemtoReg <= 0;
       C.MemRead <= 0;
       C.MemWrite <= 0;
@@ -246,6 +260,7 @@ module Datapath #(
       C.func7 <= 0;
     end else begin
       C.RegWrite <= B.RegWrite;
+      C.Jal <= B.Jal;
       C.MemtoReg <= B.MemtoReg;
       C.MemRead <= B.MemRead;
       C.MemWrite <= B.MemWrite;
@@ -283,6 +298,7 @@ module Datapath #(
     if (reset)   // initialization
         begin
       D.RegWrite <= 0;
+      D.Jal <= 0;
       D.MemtoReg <= 0;
       D.Pc_Imm <= 0;
       D.Pc_Four <= 0;
@@ -292,6 +308,7 @@ module Datapath #(
       D.rd <= 0;
     end else begin
       D.RegWrite <= C.RegWrite;
+      D.Jal <= C.Jal;
       D.MemtoReg <= C.MemtoReg;
       D.Pc_Imm <= C.Pc_Imm;
       D.Pc_Four <= C.Pc_Four;
@@ -308,7 +325,14 @@ module Datapath #(
       D.Alu_Result,
       D.MemReadData,
       D.MemtoReg,
-      WrmuxSrc
+      temp
+  );
+
+  mux2 #(32) jalmux  (
+    temp,
+    D.Pc_Four,
+    D.Jal,
+    WrmuxSrc
   );
 
   assign WB_Data = WrmuxSrc;
