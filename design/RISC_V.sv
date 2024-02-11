5cd1ac4f56d12bfd746e7706d628af1f73001fad
Andreywid Souza <130323202+Andreywid@users.noreply.github.com>
2024-02-10 21:29:35 -0300

    Att do RISC_V

--- design/RISC_V.sv
+++ design/RISC_V.sv
@@ -19,7 +19,7 @@ module riscv #(
 );
 
   logic [6:0] opcode;
-  logic ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch;
+  logic Halt,ALUSrc, Jal, MemtoReg, RegWrite, MemRead, MemWrite, Branch, Jalr;
   logic [1:0] ALUop;
   logic [1:0] ALUop_Reg;
   logic [6:0] Funct7;
@@ -28,13 +28,16 @@ module riscv #(
 
   Controller c (
       opcode,
+      Halt,
       ALUSrc,
+      Jal,
       MemtoReg,
       RegWrite,
       MemRead,
       MemWrite,
       ALUop,
-      Branch
+      Branch,
+      Jalr
   );
 
   ALUController ac (
@@ -48,11 +51,14 @@ module riscv #(
       clk,
       reset,
       RegWrite,
+      Jal,
       MemtoReg,
       ALUSrc,
       MemWrite,
       MemRead,
       Branch,
+      Halt,
+      Jalr,
       ALUop,
       Operation,
       opcode,
